aac1205d496eef85b27a2bc3a91a39f01e8213eb
Bob Thompson <doctorbobt@gmail.com>
2019-12-02 14:27:23 -0600

    Move Module 10 projects

---
+++ MSTA Projects/ConcurrentQueue/ConcurrentQueue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentQueue
+{
+    class Program
+    {
+        static ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+        static void PlaceOrders()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Thread.Sleep(250);
+                String order = String.Format("Order {0}", i);
+                queue.Enqueue(order);
+                Console.WriteLine("Added {0}", order);
+            }
+        }
+        static void ProcessOrders()
+        {
+            string order = string.Empty;
+            while (true)
+            {
+                //continue indefinitely
+                Thread.Sleep(2000);
+                if (queue.TryDequeue(out order))
+                {
+                    Console.WriteLine("Processed {0}", order);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var taskPlaceOrders = Task.Run(() => PlaceOrders());
+            Task.Run(() => ProcessOrders());
+            Task.Run(() => ProcessOrders());
+            Task.Run(() => ProcessOrders());
+            taskPlaceOrders.Wait();
+            Console.WriteLine("Press ENTER to finish");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MSTA Projects/Threadpool/Threadpool/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Threadpool
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // Queue the task.
+            // Often you won’t use the ThreadPool class directly. Instead you can use other technologies built on top of it, such as TPL, APM, Event -
+            // EAP, TAP, or the new async / await keywords.
+            ThreadPool.QueueUserWorkItem(ThreadProc);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+
+            // If you comment out the call to the Thread.Sleep method, the main thread exits before method runs on the thread pool thread. 
+            // The thread pool uses background threads, which do not keep the application running if all foreground threads have terminated. 
+
+            Console.WriteLine("Main thread exits.");
+
+            Console.ReadKey();
+        }
+
+        // This thread procedure performs the task. No state object was passed to QueueUserWorkItem, so stateInfo is null.
+        static void ThreadProc(Object stateInfo)
+        {
+            int worker = 0;
+            int io = 0;
+            ThreadPool.GetAvailableThreads(out worker, out io);
+
+            Console.WriteLine("Hello from the thread pool.");
+            Console.WriteLine("Thread pool threads available at startup: ");
+            Console.WriteLine("   Worker threads: {0:N0}", worker);
+            Console.WriteLine("   Asynchronous I/O threads: {0:N0}", io);
+        }
+    }
+}
