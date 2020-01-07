ddb5ed054f333da0a03868365df6254546501560
Bob Thompson <doctorbobt@gmail.com>
2020-01-06 18:02:36 -0600

    For Sheila

---
+++ MSTA Projects/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private dbContext _db;
+        public HomeController()
+        {
+            _db = new dbContext();
+        }
+        public IActionResult Index()
+        {
+            var modelData = new HomeIndexViewModel();
+            modelData.events = _db.events;
+            modelData.jobs = _db.jobs;
+            modelData.learnings = _db.learnings;
+            return View("Index", modelData);
+        }
+
+            public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MSTA Projects/WebApplication1/WebApplication1/Models/Class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Event
+    {
+        public string Title { get; set; }
+    }
+    public class Jobs
+    {
+        public string Title { get; set; }
+
+    }
+    public class Learning
+    {
+        public string Title { get; set; }
+
+    }
+    class dbContext
+    {
+        public List<Event> events = new List<Event>();
+        public List<Jobs> jobs = new List<Jobs>();
+        public List<Learning> learnings = new List<Learning>();
+        public dbContext()
+        {
+            events.Add(new Event() { Title = "First event" });
+            events.Add(new Event() { Title = "second event" });
+            events.Add(new Event() { Title = "third event" });
+            jobs.Add(new Jobs() { Title = "First job" });
+            jobs.Add(new Jobs() { Title = "second job" });
+            jobs.Add(new Jobs() { Title = "third job" });
+            learnings.Add(new Learning() { Title = "First learning" });
+            learnings.Add(new Learning() { Title = "second learning" });
+            learnings.Add(new Learning() { Title = "third learning" });
+        }
+    }
+    public class HomeIndexViewModel
+    {
+        public List<Event> events;
+        public List<Jobs> jobs;
+        public List<Learning> learnings;
+
+    }
+
+}
